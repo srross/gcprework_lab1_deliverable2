b33043a4987d03f2012d090fc158d2ee5d4ec17b
Ross, Sonia <soniaross@amrock.com>
2022-01-06 12:03:15 -0500

    removing duplicates

---
+++ SRossGCDeliverable2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SRossGCDeliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxPpartySize = 6;
+            int minPartySize = 1;
+            int[] inputPartySize = new int[6] { 0, 0, 0, 0, 0, 0 };
+
+            decimal buffetPrice;
+
+            string drink1;
+            int countDrink1;
+            decimal costDrink1;
+            decimal totalCostDrink1;
+
+            string drink2;
+            int countDrink2;
+            decimal costDrink2;
+            decimal totalCostDrink2;
+
+            decimal totalPrice;
+
+            Console.WriteLine("Welcome to Grand Circus Buffet! We can accomodate groups of 6 people or less.  How many people are in your party today?");
+            Console.ReadLine();
+            
+            foreach(int person in inputPartySize)
+            {
+
+            }
+        }
+    }
+}
