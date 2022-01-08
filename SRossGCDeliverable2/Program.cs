3a9e6320d935108ac78ca7e273577ca46757c991
Ross, Sonia <soniaross@amrock.com>
2022-01-08 16:50:18 -0500

    GCprework_Lab1_deliverable2 srross/deliverable_2

--- SRossGCDeliverable2/Program.cs
+++ SRossGCDeliverable2/Program.cs
@@ -2,35 +2,102 @@
 
 namespace SRossGCDeliverable2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int maxPpartySize = 6;
-            int minPartySize = 1;
-            int[] inputPartySize = new int[6] { 0, 0, 0, 0, 0, 0 };
+            const string optOne = "sake";
+            const string optTwo = "shochu";
+            const decimal buffetPrice = 9.99m;
+            const decimal optOnePrice = 6.66m;
+            const decimal optTwoPrice = 3.33m;
 
-            decimal buffetPrice;
+            string emptyGlass = "empty glass";
+            decimal totalBill = 0.00m;
 
-            string drink1;
-            int countDrink1;
-            decimal costDrink1;
-            decimal totalCostDrink1;
+            int optOneCount = 0;
+            int optTwoCount = 0;
+            int emptyGlassCount = 0;
+                       
+            Console.Write("Welcome to Cherry Blossom Buffet, how many will be joining your party? ");
+            string input = Console.ReadLine();
+            int guestCount = 0;
+            bool isValidGuestCount = (int.TryParse(input, out guestCount)
+                                    && guestCount >= 1 && guestCount <=6 );
 
-            string drink2;
-            int countDrink2;
-            decimal costDrink2;
-            decimal totalCostDrink2;
+           
 
-            decimal totalPrice;
-
-            Console.WriteLine("Welcome to Grand Circus Buffet! We can accomodate groups of 6 people or less.  How many people are in your party today?");
-            Console.ReadLine();
-            
-            foreach(int person in inputPartySize)
+            if (!isValidGuestCount)
             {
+                Console.WriteLine("I'm sorry, we're not able to accomodate your party today.");
+                Console.ReadLine();
+            }
+            else //if(isValidGuestCount)
+            {
+                int[] guests = new int[guestCount - 1];
 
+                Console.Write("Party of " + input + ". Please, right this way."
+                                + "Can I start you off with something to drink?"
+                                + "Would you like to try our house Sake or Shochu. ");
+
+                Console.WriteLine();
+
+                for (int i = 0; i <= guests.Length; i++)
+                {
+                    string guest = "Guest " + (i + 1).ToString();
+                    Console.Write(guest + " what will you have today? ");
+                    string order = Console.ReadLine().ToLower().Trim();
+
+                    switch (order)
+                    {
+                        case optOne:
+                            optOneCount = optOneCount + 1;
+                            Console.WriteLine(order + "! Excellent Choice!");
+                            Console.WriteLine();
+
+                            decimal subtotal1 = buffetPrice + optOnePrice;
+                            totalBill = totalBill + subtotal1;
+                            break;
+
+
+                        case optTwo:
+                            optTwoCount = optTwoCount + 1;
+                            Console.WriteLine(order + "! Excellent Choice!");
+                            Console.WriteLine();
+
+                            decimal subtotal2 = buffetPrice + optTwoPrice;
+                            totalBill = totalBill + subtotal2;
+                            break;
+
+                        default:
+                            emptyGlassCount = emptyGlassCount + 1;
+                            Console.WriteLine("Sorry, we're not able to offer that today." 
+                                                + "I'll bring you an empty glass if you want to help yourself to the free water dispenser at the buffet. ");
+                            Console.WriteLine();
+
+                            decimal subtotal3 = buffetPrice;
+                            totalBill = totalBill + subtotal3;
+                            break;
+                    }
+                }
+
+                string printedBill = String.Format("{0:0.##}", totalBill.ToString());
+
+                Console.WriteLine("Ok, I have: ");
+                Console.WriteLine("     " + optOneCount + " " + optOne);
+                Console.WriteLine("     " + optTwoCount + " " + optTwo);
+                Console.WriteLine("     " + emptyGlassCount + " " + emptyGlass);
+                Console.WriteLine("Coming right up!");
+                Console.WriteLine();
+                Console.WriteLine("Your total bill is $" + totalBill + ".");
+                Console.WriteLine();
+                Console.WriteLine("I hope you enjoy today's buffet selections.");
+                Console.WriteLine("Please help yourself, and I'll be right back with your drinks.");
+                Console.ReadLine();
             }
         }
     }
 }
+
+// Need to account for zero count on either option, 
+// and default option before printing total order and bill.
